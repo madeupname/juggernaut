b7aa7d18bbe8c3ec306bb477adc9c9edb8ef8b44
Philip Yurchuk <yurchp@yahoo.com>
2012-03-18 17:00:44 -0700

    Removed maxSize constraint from Meeting description. Added comments. Issue #1 http://github.com/madeupname/juggernaut/issues/issue/1

--- grails-app/domain/ocjug/juggernaut/Meeting.groovy
+++ grails-app/domain/ocjug/juggernaut/Meeting.groovy
@@ -2,12 +2,18 @@ package ocjug.juggernaut
 
 import java.sql.Timestamp
 
+/**
+ * Represents a user group meeting.
+ * 
+ * @author Philip Yurchuk
+ *
+ */
 class Meeting {
 
     static constraints = {
 		title blank: false
 		speaker blank: false
-		description size: 5..500, blank: false
+		description minSize: 5, blank: false
 		meetingDate min: (new Date()).minus(1) // earliest date must be within 24h
 		startTime matches: "[0-9]+:[0-9]+(AM|PM)?", blank: false
 		endTime matches: "[0-9]+:[0-9]+(AM|PM)?", blank: false
@@ -17,9 +23,10 @@ class Meeting {
 	String speaker
 	String description
 	
+	// TODO: Remove meetingDate, change start and end times to Date to support exporting to calendars.
 	Date meetingDate
-	String startTime
-	String endTime
+	String startTime 
+	String endTime 
 	
 	Location location
 }
