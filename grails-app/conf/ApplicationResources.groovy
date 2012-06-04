46210e4756ee916aea9dfb6e961a6000cf2b2908
Philip <yurchp@yahoo.com>
2012-06-04 16:15:54 -0700

    Added lesscss-resources plugin and Twitter Bootstrap files. Configured them and updated templates to use them. Issue #10

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,26 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+	
+	'bootstrap-js' {
+		dependsOn 'jquery'
+		resource url:'twitter-bootstrap/js/bootstrap-alert.js'
+		resource url:'twitter-bootstrap/js/bootstrap-button.js'
+		resource url:'twitter-bootstrap/js/bootstrap-carousel.js'
+		resource url:'twitter-bootstrap/js/bootstrap-collapse.js'
+		resource url:'twitter-bootstrap/js/bootstrap-dropdown.js'
+		resource url:'twitter-bootstrap/js/bootstrap-modal.js'
+		resource url:'twitter-bootstrap/js/bootstrap-popover.js'
+		resource url:'twitter-bootstrap/js/bootstrap-scrollspy.js'
+		resource url:'twitter-bootstrap/js/bootstrap-tab.js'
+		resource url:'twitter-bootstrap/js/bootstrap-tooltip.js'
+		resource url:'twitter-bootstrap/js/bootstrap-transition.js'
+		// left out typeahead, no plans for it
+	} 
+
+	// includes bootstrap and responsive bootstrap
+	'custom-bootstrap' {
+		dependsOn 'bootstrap-js'
+		resource url:[dir: 'twitter-bootstrap/less', file: 'custom-bootstrap.less'], attrs:[rel: "stylesheet/less", type:'css']
+	}
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,8 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
-
+		compile ":lesscss-resources:1.3.0.3"
+		
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
